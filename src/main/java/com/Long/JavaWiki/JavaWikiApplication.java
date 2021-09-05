cbadd7207a8a703a75436c3228fdb4264c72e8c4
yuanjunlong <792516830@qq.com>
2021-09-05 21:08:07 +0800

    初始化项目

---
+++ src/main/java/com/Long/JavaWiki/JavaWikiApplication.java
@@ -0,0 +1,13 @@
+package com.Long.JavaWiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Long/JavaWiki/JavaWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Long.JavaWiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

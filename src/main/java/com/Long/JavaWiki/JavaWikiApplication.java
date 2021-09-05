63cc9b8ba4522493e26553ca2fa3f3f9a6fcd2a6
yuanjunlong <792516830@qq.com>
2021-09-05 21:53:33 +0800

    增加启动信息

--- src/main/java/com/Long/JavaWiki/JavaWikiApplication.java
+++ src/main/java/com/Long/JavaWiki/JavaWikiApplication.java
@@ -1,13 +1,20 @@
 package com.Long.JavaWiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class JavaWikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(JavaWikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(JavaWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }

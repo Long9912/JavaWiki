c84c729aebe5942459118f20751a47a4dc879bc2
yuanjunlong <792516830@qq.com>
2021-09-06 15:07:30 +0800

    配置Druid后台监控

--- src/main/java/com/Long/JavaWiki/controller/TestController.java
+++ src/main/java/com/Long/JavaWiki/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.Long.JavaWiki.controller;
 
+import com.Long.JavaWiki.mapper.UserMapper;
+import com.Long.JavaWiki.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private UserMapper userMapper;
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello world!";
+    public List<User> hello(){
+        List<User> userList = userMapper.selectList(null);
+        return userList;
     }
 }

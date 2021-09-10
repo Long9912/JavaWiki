4bf17234bcde75f736349ed58e8dbaebd24185f4
yuanjunlong <792516830@qq.com>
2021-09-10 16:34:40 +0800

    电子书管理表格展示

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home.vue'
+import About from '../views/about.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +12,17 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    //component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
+  },
 ]
 
 const router = createRouter({
---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <a-layout style="padding: 0 24px 24px">
+      <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+        <a-table
+            :columns="columns"
+            :row-key="record => record.id"
+            :data-source="ebooks"
+            :pagination="pagination"
+            :loading="loading"
+            @change="handleTableChange"
+        >
+          <template #cover="{ text: cover }">
+            <img v-if="cover" :src="cover" alt="avatar"/>
+          </template>
+          <template v-slot:action="{ text, record }">
+            <a-space size="small">
+              <a-button type="primary">
+                编辑
+              </a-button>
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-space>
+          </template>
+        </a-table>
+      </a-layout-content>
+    </a-layout>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue';
+import axios from "axios";
+import {message} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const param = ref();
+    param.value = {};
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 2,
+      total: 0
+    });
+    const loading = ref(false);
+
+    const columns = [
+      {
+        title: '封面',
+        dataIndex: 'cover',
+        slots: {customRender: 'cover'}
+      },
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '分类1',
+        dataIndex: 'category1Id'
+      },
+      {
+        title: '分类2',
+        dataIndex: 'category2Id'
+      },
+      {
+        title: '文档数',
+        dataIndex: 'docCount'
+      },
+      {
+        title: '阅读数',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
+      }
+    ];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      axios.get("/ebook/list", params).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.code === 2000) {
+          ebooks.value = data.content;
+          // 重置分页按钮
+          pagination.value.current = params.page;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    /**
+     * 表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({});
+    });
+
+    return {
+      param,
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange
+    }
+  }
+});
+</script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>

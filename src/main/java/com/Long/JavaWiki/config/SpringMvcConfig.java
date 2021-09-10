89ac38bc79eea732f05a401793b176becfc097c4
yuanjunlong <792516830@qq.com>
2021-09-10 11:18:23 +0800

    增加拦截器,打印接口耗时

---
+++ src/main/java/com/Long/JavaWiki/config/SpringMvcConfig.java
@@ -0,0 +1,23 @@
+package com.Long.JavaWiki.config;
+
+import com.Long.JavaWiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/user/login"
+                );
+    }
+}

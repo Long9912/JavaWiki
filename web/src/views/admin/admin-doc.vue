114b157c9b024f4a7a52f9ff80cc2bd1a5a7525f
yuanjunlong <792516830@qq.com>
2021-09-13 16:27:07 +0800

    在选择父节点时,使用递归算法,将树形结构中,当前目标节点以及子孙节点设置成不可选状态

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -67,12 +67,16 @@
           <a-input v-model:value="doc.name"/>
         </a-form-item>
         <a-form-item label="父文档">
-          <a-select ref="select" v-model:value="doc.parent">
-            <a-select-option value="0">无</a-select-option>
-            <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="doc.id === c.id">
-              {{c.name}}
-            </a-select-option>
-          </a-select>
+          <a-tree-select
+              v-model:value="doc.parent"
+              style="width: 100%"
+              :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+              :tree-data="treeSelectData"
+              placeholder="请选择父文档"
+              tree-default-expand-all
+              :replaceFields="{title: 'name', key:'id',value:'id'}"
+          >
+          </a-tree-select>
         </a-form-item>
         <a-form-item label="顺序">
           <a-input v-model:value="doc.sort"/>
@@ -161,9 +165,40 @@ export default defineComponent({
     };
 
     //----------表单-----------
+    // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
+    const treeSelectData = ref();
+    treeSelectData.value=[];
     const doc = ref({});
     const modalVisible = ref<boolean>(false);
     const modalLoading = ref<boolean>(false);
+
+    /**
+     * 将某节点及其子孙节点全部置为disabled
+     */
+    const setDisable = (treeSelectData: any, id: any) => {
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id == id) {
+          // 如果当前节点就是目标节点,将目标节点设置为disabled
+          node.disabled = true;
+
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            setDisable(children, id);
+          }
+        }
+      }
+    };
 
     /**
      * 新增
@@ -171,6 +206,10 @@ export default defineComponent({
     const add = () => {
       modalVisible.value = true;
       doc.value = {};
+
+      treeSelectData.value = Tool.copy(level1.value);
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     /**
@@ -180,6 +219,12 @@ export default defineComponent({
       modalVisible.value = true;
       //复制当前列的参数
       doc.value = Tool.copy(record);
+      // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value, record.id);
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     const handleModalOk = () => {
@@ -223,6 +268,7 @@ export default defineComponent({
       add,
       edit,
       doc,
+      treeSelectData,
       modalVisible,
       modalLoading,
       handleModalOk,

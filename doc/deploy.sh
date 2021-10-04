7c733d86700cf422d63a5d6265e62fac40e77331
yuanjunlong <792516830@qq.com>
2021-10-05 00:35:07 +0800

    配置Nginx后端反向代理

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "publish----------"
+echo "---publish---"
 
 process_id=`ps -ef | grep JavaWiki.jar | grep -v grep |awk '{print $2}'`
 if [ $process_id ] ; then
@@ -8,5 +8,7 @@ fi
 
 source /etc/profile
 nohup java -jar -Dspring.profiles.active=prod ~/wiki/JavaWiki.jar > /dev/null 2>&1 &
+redis-server /usr/local/redis/bin/conf/redis.conf
+service nginx start
 
 echo "end publish"

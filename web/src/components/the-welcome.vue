f0af5d647c538a97f854941c0ef365998dc42c93
yuanjunlong <792516830@qq.com>
2021-10-05 16:09:32 +0800

    修复部署后问题：切到别的页面，再切回首页，图表显示不出来

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -89,7 +89,7 @@
   </a-row>
   <br>
   <a-row>
-    <a-col :span="24">
+    <a-col :span="24" id="main-col">
       <div id="main" style="width:100%; height: 300px"></div>
     </a-col>
   </a-row>
@@ -147,6 +147,12 @@ export default defineComponent({
     }
 
     const init30DayStatistic = (list:any) => {
+      // 发布生产后出现问题：切到别的页面，再切回首页，报表显示不出来
+      // 解决方法：把原来的id=main的区域清空，重新初始化
+      const mainDom = document.getElementById('main-col');
+      if (mainDom) {
+        mainDom.innerHTML = '<div id="main" style="width: 100%;height:300px;"></div>';
+      }
       // 基于准备好的dom，初始化echarts实例
       const myChart = echarts.init(document.getElementById('main'));
 
